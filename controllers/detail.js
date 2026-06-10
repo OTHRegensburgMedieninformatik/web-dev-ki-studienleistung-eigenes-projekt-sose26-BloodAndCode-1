d15ec61417983db1ea383311aee56c2ea9b097eb
Alona R <raygorodetskaya@gmail.com>
2026-06-10 16:27:19 +0200

    Release 4: Leaflet map and weather on detail page

--- controllers/detail.js
+++ controllers/detail.js
@@ -1,6 +1,7 @@
 const logger = require("../utils/logger.js");
 const db = require("../db/db.js");
 const accounts = require("./accounts.js");
+const { getWeekendWeather } = require("../utils/weather.js");
 
 const destinations = {
   async show(request, response) {
@@ -11,9 +12,12 @@ const destinations = {
       "SELECT * FROM destinations WHERE id = $1",
       [request.params.id]
     );
+    const destination = result.rows[0];
+    const weather = await getWeekendWeather(destination.lat, destination.lon);
     const viewData = {
-      title: "Reiseziel",
-      destination: result.rows[0],
+      title: destination.name,
+      destination: destination,
+      weather: weather,
       user: user,
     };
     response.render("detail", viewData);
